6d6834506fd26b0b9018422fca11795118cee35e
Hasimy <ahmadhasyimi674@gmail.com>
2019-05-30 13:00:23 +0700

    Initial commit

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+
+//settings
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+//render & port
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(port, () => {
+  console.log(`Server in localhost:${port}`);
+});
